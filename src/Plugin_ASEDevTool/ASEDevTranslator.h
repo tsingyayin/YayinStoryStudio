1f293dc978430547bd35eb514cc46409dc4e082f
青雅音 <tsingyayin@qq.com>
2026-04-13 03:03:28 +0800

    删除弃用的ASEDevTool插件

--- src/Plugin_ASEDevTool/ASEDevTranslator.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <General/Translator.h>
-
-class ASEDevTranslator :public Visindigo::General::Translator {
-public:
-	ASEDevTranslator(Visindigo::General::Plugin* parent);
-};
--- src/Plugin_ASEDevTool/FileServer/FS_AStory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Editor/FileServer.h>
-#include <Editor/EditorPlugin.h>
-
-class AStoryFileServer : public YSSCore::Editor::FileServer {
-public:
-	AStoryFileServer(YSSCore::Editor::EditorPlugin* plugin);
-	~AStoryFileServer();
-};
