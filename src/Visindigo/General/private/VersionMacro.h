e05d99defa37433e30a28f5d016272cdc24e5aff
青雅音 <tsingyayin@qq.com>
2026-08-25 21:19:25 +0800

    修复编译配置和版本号错误

--- src/Visindigo/General/private/VersionMacro.h
+++ src/Visindigo/General/private/VersionMacro.h
@@ -12,10 +12,10 @@
 
 // Visindigo version macro
 #define Visindigo_VERSION_MAJOR 0
-#define Visindigo_VERSION_MINOR 16
-#define Visindigo_VERSION_PATCH 2
+#define Visindigo_VERSION_MINOR 17
+#define Visindigo_VERSION_PATCH 0
 
-#define Visindigo_ABI_VERSION_MAJOR 16
-#define Visindigo_ABI_VERSION_MINOR 1
+#define Visindigo_ABI_VERSION_MAJOR 17
+#define Visindigo_ABI_VERSION_MINOR 0
 
 #endif // Visindigo_General_private_VersionMacro_h
